6cb57c9d8420a7fe7c032a68399083e86f6b3a87
Ignacio Hermosilla <ignacio@seahorse.co>
2015-08-30 13:43:07 -0300

    added hashlib

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     keywords=['oneclick', 'one_click', 'transbank', 'webpay', 'chile', 'payments'],
     install_requires=[
         'pytz',
+        'hashlib',
         'PySimpleSOAP==1.16',
         'arrow==0.6.0',
         'future==0.12.3',
